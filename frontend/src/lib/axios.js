9c2a2c6c7df197c4688ea744ec6acd120a9f1d65
nikhil <nikhilyagik2004@gmail.com>
2025-01-01 20:53:24 +0530

    localhost url fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstant=axios.create({
-    baseURL: import.meta.env.MODE==="development"?"http://localhost:8000/api/v1":"/api",
+    baseURL: import.meta.env.MODE==="development"?"http://localhost:8000/api/":"/api",
     withCredentials:true,
 })
